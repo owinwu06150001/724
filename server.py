e662f5eefd04d389dbe730daf800cfc9f5be7081
owinwu06150001 <owinwu0615@gmail.com>
2026-02-09 23:33:55 +0800

    Update server.py

--- server.py
+++ server.py
@@ -2,17 +2,18 @@ from flask import Flask
 from threading import Thread
 import os
 
-app = Flask("")
+app = Flask('')
 
-@app.route("/")
+@app.route('/')
 def home():
-    return "Bot is running 24/7"
+    return "Bot is running and staying alive!"
 
 def run():
-    # 這裡使用 Render 要求的 Port
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    # Koyeb 會自動分配 PORT，必須監聽 0.0.0.0
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
     t = Thread(target=run)
+    t.daemon = True # 確保主程式結束時執行緒也會關閉
     t.start()
